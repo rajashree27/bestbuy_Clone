5350e79b6a9371b7c82bc1c3c1698309b0e11297
rajashree27 <rjchakraborty98@gmail.com>
2022-04-02 17:55:44 +0530

    cart completed - add and remove from cart

--- cart.js
+++ cart.js
@@ -1,17 +1,16 @@
+// document.querySelector("#cartItemsList").innerHTML="";
 var cartData = JSON.parse(localStorage.getItem("cartItems"));
-console.log(cartData);
+
+var flag=1;
 
 var total=document.createElement("div");
-total.innerText=total.innerText.substring(1);
 
-console.log(typeof(total.innerText))
 total.innerText=cartData.reduce(function(sum,ele,ind,arr)
 {
     // object price name key is "price"
-    return sum+Number(ele.itemPrice)
+    return Math.floor(sum+Number(ele.prod_price))
 },0);
 
-console.log(total.innerText)
 
 document.querySelector("#price").innerHTML="";
 document.querySelector("#price").append("$"+total.innerText)
@@ -20,27 +19,60 @@ cartData.map(function(el,ind)
 {
     var box=document.createElement("div");
 
-    var divHead=document.createElement("p");
-    divHead.innerText="Cart Items:";
-    divHead.style="font-size:25px;"
-
-    var line=document.createElement("hr");
-
     var imgItm=document.createElement("img")
-    imgItm.src=el.itemPhoto;
-    // imgItm.src="https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6401/6401735_sd.jpg;maxHeight=640;maxWidth=250"
-
+    imgItm.src=el.prod_url;
+    imgItm.style="height:100px;width:150px"
+   
     var nameItm=document.createElement("p")
-    nameItm.innerText=el.itemCat;
-    nameItm.style="color:blue;"
+    nameItm.innerText=el.prod_model;
+    nameItm.style="color:blue;text-decoration:underline;cursor:pointer"
+
+    var removeItm=document.createElement("button");
+    removeItm.innerText="Remove from cart";
+    removeItm.style="cursor:pointer;color:white;background-color:#0b4ec0;border:none;padding:5px 10px;border-radius:5px";
+    removeItm.addEventListener("click",function(){removeFromCart(el,ind)})
 
     var priceItm=document.createElement("p")
-    priceItm.innerText="$"+el.itemPrice;
+    priceItm.innerText="$"+el.prod_price;
+
+    if(flag==1)
+    {
+        document.querySelector("#cartItemsList").innerHTML="";
+        var nameofCartItems=document.createElement("div");
+
+        var divHead=document.createElement("p");
+        divHead.innerText="Cart Items:";
+        divHead.style="font-size:25px;"
+
+        var line=document.createElement("hr");
+
+        nameofCartItems.append(divHead,line)
+
+        box.append(nameofCartItems,imgItm,nameItm,priceItm,removeItm);
+        flag++;
+    }
+    else{
+        var nameofCartItems=document.createElement("div");
+
+        var divHead=document.createElement("p");
+        divHead.innerText="Cart Items";
+        divHead.style="font-size:25px; color:white"
+
+        var line=document.createElement("hr");
+
+        nameofCartItems.append(divHead,line)
 
 
-    box.append(divHead,line,imgItm,nameItm,priceItm);
+        box.append(nameofCartItems,imgItm,nameItm,priceItm,removeItm);
+    }
     
-    document.querySelector("#cartItemsList").innerHTML="";
     document.querySelector("#cartItemsList").append(box);
 
 })
+
+function removeFromCart(el,ind)
+{
+    cartData.splice(ind,1);
+    localStorage.setItem("cartItems",JSON.stringify(cartData));
+    window.location.reload();
+}
