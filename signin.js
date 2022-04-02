a7ccec5c77b19a322d9b530e59ce21539bba8d37
Aakash928 <asassss337@gmail.com>
2022-04-02 15:08:58 +0530

    signin and signup added

---
+++ signin.js
@@ -0,0 +1,48 @@
+var redirectregister = document.querySelector("#redirectRegister").addEventListener("click", timemachine);
+var collectregistereddata = JSON.parse(localStorage.getItem("registereduser"));
+
+function timemachine() {
+    window.location.href = "signup.html";
+}
+var loginuserdetails = document.querySelector("form").addEventListener("submit", loginfunc);
+
+function loginfunc() {
+    event.preventDefault();
+    var useremail = document.querySelector("#email").value;
+    var userpassword = document.querySelector("#password").value;
+     var flag=false;
+    // console.log(userpassword, useremail);
+    var dangeremail = document.querySelector(".email");
+    var dangerpassword = document.querySelector(".password");
+    var dangernotexist = document.querySelector(".notexist");
+    collectregistereddata.map(function(el) {
+
+        if (el.email == useremail && el.password == userpassword) {
+            alert("login sucessfull press ok to shopping")
+            window.location.href = "index.html";
+            document.querySelector("#email").value="";
+           document.querySelector("#password").value="";
+            // console.log(el.email, useremail);
+            // console.log(el.password, userpassword);
+        }
+        if (el.email != useremail && el.password != userpassword) {
+            dangernotexist.style.display = "block";
+
+        } else {
+            dangernotexist.style.display = "block";
+            // dangernotexist.style.display = "block";
+        }
+        if (useremail == "") {
+            dangeremail.style.display = "block";
+        } else {
+            dangeremail.style.display = "none";
+        }
+        if (userpassword == "") {
+            dangerpassword.style.display = "block";
+        } else {
+            dangerpassword.style.display = "none";
+        }
+    });
+
+
+}
---
+++ signup.js
@@ -0,0 +1,69 @@
+document.querySelector("form").addEventListener("submit", usersubmit);
+var registereduser = JSON.parse(localStorage.getItem("registereduser")) || [];
+
+function usersubmit() {
+    event.preventDefault();
+    var name = document.querySelector("#name").value;
+    var email = document.querySelector("#email").value;
+    var re_email = document.querySelector("#confirm-email").value;
+    var password = document.querySelector("#password").value;
+    var re_password = document.querySelector("#confirm-password").value;
+    var mobile = document.querySelector("#mobile").value;
+
+
+    var obj = {
+        name: name,
+        email: email,
+        remail: re_email,
+        password: password,
+        repassword: re_password,
+        mobile: mobile,
+   
+    }
+
+    // console.log(obj);
+    var dangerName = document.querySelector(".name");
+    var dangerEmail = document.querySelector(".email");
+    var dangerPassword = document.querySelector(".password");
+    var dangerRepassword = document.querySelector(".repassword");
+    var dangerReEmail = document.querySelector(".remail");
+   
+    if (name == "") {
+        dangerName.style.display = "block";
+    } else {
+        dangerName.style.display = "none";
+    }
+    if (email == "") {
+        dangerEmail.style.display = "block";
+    } else {
+        dangerEmail.style.display = "none";
+    }
+    if (password == "") {
+        dangerPassword.style.display = "block";
+    } else {
+        dangerPassword.style.display = "none";
+    }
+    if (password != re_password) {
+        dangerRepassword.style.display = "block";
+    } else {
+        dangerRepassword.style.display = "none";
+    }
+    if (email != re_email) {
+        dangerReEmail.style.display = "block";
+    } else {
+        dangerReEmail.style.display = "none";
+    }
+
+    if (name!= "" && email!= "" && password!="" && re_password!="") {
+      /*   console.log("sdfg") */
+      registereduser.push(obj);
+      localStorage.setItem("registereduser", JSON.stringify(registereduser));
+      window.location.href = "signin.html";
+      
+    } else {
+       /*  window.localStorage.clear(); */
+      
+    }
+}
+
+// localStorage.clear();
